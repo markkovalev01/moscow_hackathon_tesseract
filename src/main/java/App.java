35e5d137fdc50fac1ab53c2ce823ab5dca33b4c5
Mark <kovalevm9609@yandex.ru>
2020-10-31 17:23:09 +0900

    init commit

---
+++ src/main/java/App.java
@@ -0,0 +1,64 @@
+import com.fasterxml.jackson.databind.ObjectMapper;
+import lombok.SneakyThrows;
+import net.sourceforge.tess4j.Tesseract;
+import net.sourceforge.tess4j.TesseractException;
+import org.apache.commons.io.FilenameUtils;
+import utils.FileUtils;
+
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class App {
+
+    @SneakyThrows
+    public static void main(String[] args) {
+
+        Tesseract tesseract = new Tesseract();
+        tesseract.setDatapath("src/main/resources/tessdata");
+        tesseract.setLanguage("rus");
+        tesseract.setPageSegMode(1);
+        tesseract.setOcrEngineMode(1);
+        Pattern spacePattern = Pattern.compile("[\\s\\n]+");
+        FileUtils.getFiles("Dataset/Разр. на ввод").forEach(dir -> {
+            dir.forEach(file -> {
+                List<BufferedImage> bufferedImageList = FileUtils.fileToBuffImg(file);
+                HashMap<String, String> pages = new HashMap<>();
+                bufferedImageList.forEach(bi -> {
+                    try {
+                        long startTime = System.nanoTime();
+                        String result = tesseract.doOCR(bi);
+                        Matcher spaceMatcher = spacePattern.matcher(result);
+                        String newS = spaceMatcher.replaceAll(" ");
+                        long endTime = System.nanoTime();
+                        long timeElapsed = endTime - startTime;
+                        System.out.println(timeElapsed);
+                        System.out.println("Execution time in milliseconds : " +
+                                timeElapsed / 1000000);
+                        pages.put("page" + bufferedImageList.indexOf(bi), newS);
+                        ObjectMapper mapper = new ObjectMapper();
+                        try {
+                            mapper.writeValue(new File(FilenameUtils.removeExtension(file) + ".json"), pages);
+
+                        } catch (IOException e) {
+                            e.printStackTrace();
+                        }
+                    } catch (TesseractException e) {
+                        e.printStackTrace();
+                    }
+
+                });
+            });
+        });
+
+
+//
+//
+
+
+    }
+}
---
+++ src/main/java/utils/FileUtils.java
@@ -0,0 +1,69 @@
+package utils;
+
+import lombok.SneakyThrows;
+import org.apache.commons.io.FilenameUtils;
+import org.apache.pdfbox.pdmodel.PDDocument;
+import org.apache.pdfbox.rendering.ImageType;
+import org.apache.pdfbox.rendering.PDFRenderer;
+
+import javax.imageio.ImageIO;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class FileUtils {
+
+
+    @SneakyThrows
+    public static List<BufferedImage> fileToBuffImg(String filePath) {
+        if (FilenameUtils.getExtension(filePath.toLowerCase()).equals("pdf")) {
+            return pdfToBuffImg(filePath);
+        } else {
+            return imgToBuffImg(filePath);
+        }
+    }
+
+    @SneakyThrows
+    private static List<BufferedImage> imgToBuffImg(String filePath) {
+        List<BufferedImage> bufferedImages = new ArrayList<BufferedImage>();
+        bufferedImages.add(ImageIO.read(new File(filePath)));
+        return bufferedImages;
+    }
+
+    @SneakyThrows
+    private static List<BufferedImage> pdfToBuffImg(String filePath) {
+        PDDocument document = PDDocument.load(new File(filePath));
+        PDFRenderer pdfRenderer = new PDFRenderer(document);
+        List<BufferedImage> res = new ArrayList<BufferedImage>();
+        for (int page = 0; page < document.getNumberOfPages(); ++page) {
+            BufferedImage bim = pdfRenderer.renderImageWithDPI(
+                    page, 300, ImageType.RGB);
+            res.add(bim);
+        }
+        document.close();
+        return res;
+    }
+
+    @SneakyThrows
+    public static List<List<String>> getFiles(String path) {
+        File file = new File(path);
+        List<List<String>> res = new ArrayList<>();
+        if (file.isDirectory()) {
+            if (!file.listFiles()[0].isDirectory()) {
+                res.add(Arrays.stream(file.listFiles()).map(File::getPath).collect(Collectors.toList()));
+                return res;
+            }
+            Arrays.stream(file.listFiles()).forEach(dir -> {
+                if (dir.isDirectory()) {
+                    res.add(Arrays.stream(dir.listFiles()).map(f -> {
+                        return f.getPath();
+                    }).collect(Collectors.toList()));
+                }
+            });
+        }
+        return res;
+    }
+}
